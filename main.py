5dbb660d84fe78c9ba204aa70f302cafd8a390f4
Emreee99 <emreee.t99@gmail.com>
2023-02-13 15:04:44 +0300

    Added dataframe

--- main.py
+++ main.py
@@ -20,14 +20,15 @@ st.markdown( f"""
     background-image: linear-gradient(90deg, rgba(40,94,174,255), rgba(20,190,241,255));
     z-index: 999990;
 }}
+    p {{
+    font-size: 1.5rem !important;
+}}
 </style>
 """, unsafe_allow_html=True)
 
 holland_logo = Image.open('./images/holland.png')
 
-
-st.image(holland_logo, width=300)
-
+st.image(holland_logo, width=350)
 
 st.text("")
 drs05_tab, dr42_tab = st.tabs(["DRS05", "DR42"])
@@ -37,88 +38,91 @@ with drs05_tab:
     for uploaded_file_drs05 in uploaded_files_drs05:
         try:
             data = pd.read_csv(uploaded_file_drs05)
-            st.write("filename:", uploaded_file_drs05.name)
+            if(data["Radar Message"][0][0] != "#"):
+                data_first = data.copy()
+                st.write("filename:", uploaded_file_drs05.name)
 
-            data["Speed"] = 0
-            data["Distance"] = 0
-            data["Status Byte"] = 0
+                data["Speed"] = 0
+                data["Distance"] = 0
+                data["Status Byte"] = 0
 
-            data = data.drop("Unnamed: 2", axis=1)
+                for i in range(len(data)):
+                    speed = data["Radar Message"][i][4:8]
+                    speed = int(speed, base=16)
 
-            for i in range(len(data)):
-                speed = data["Radar Message"][i][4:8]
-                speed = int(speed, base=16)
+                    speed = (speed * 600) / 65535
+                    data["Speed"][i] = speed
 
-                speed = (speed * 600) / 65535
-                data["Speed"][i] = speed
+                    distance_counter = data["Radar Message"][i][8:14]
+                    distance_counter = int(distance_counter, base=16)
+                    distance_counter = distance_counter / 100
+                    data["Distance"][i] = distance_counter
 
-                distance_counter = data["Radar Message"][i][8:14]
-                distance_counter = int(distance_counter, base=16)
-                distance_counter = distance_counter / 100
-                data["Distance"][i] = distance_counter
+                    status_byte = data["Radar Message"][i][22:24]
+                    ini_string = status_byte
+                    n = int(ini_string, 16)
+                    bStr = ''
+                    while n > 0:
+                        bStr = str(n % 2) + bStr
+                        n = n >> 1
+                    res = bStr
 
-                status_byte = data["Radar Message"][i][22:24]
-                ini_string = status_byte
-                n = int(ini_string, 16)
-                bStr = ''
-                while n > 0:
-                    bStr = str(n % 2) + bStr
-                    n = n >> 1
-                res = bStr
+                    data["Status Byte"][i] = "0x" + data["Radar Message"][i][22:24] + ", " + "0b" + res
 
-                data["Status Byte"][i] = res
+                counter = 0
 
-            counter = 0
+                for j in list(data["Timestamp"].unique()):
+                    for i in range(len(data.loc[data['Timestamp'] == j])):
+                        time = data["Timestamp"][counter].split(" ")[3].split(":")
+                        split_second = 60 / (len(data.loc[data['Timestamp'] == j]) + i)
+                        date = datetime.datetime(int(data["Timestamp"][counter].split(" ")[-1]),
+                                                 2,
+                                                 int(data["Timestamp"][counter].split(" ")[2]),
+                                                 int(time[0]),
+                                                 int(time[1]),
+                                                 int(time[2]),
+                                                 int((split_second * 10000) * i))
 
-            for j in list(data["Timestamp"].unique()):
-                for i in range(len(data.loc[data['Timestamp'] == j])):
-                    time = data["Timestamp"][counter].split(" ")[3].split(":")
-                    split_second = 60 / (len(data.loc[data['Timestamp'] == j]) + i)
-                    date = datetime.datetime(int(data["Timestamp"][counter].split(" ")[-1]),
-                                             2,
-                                             int(data["Timestamp"][counter].split(" ")[2]),
-                                             int(time[0]),
-                                             int(time[1]),
-                                             int(time[2]),
-                                             int((split_second * 10000) * i))
+                        data["Timestamp"][counter] = date
 
-                    data["Timestamp"][counter] = date
-
-                    counter += 1
-
-
-            data['Status Byte'] = data['Status Byte'].astype(str)
+                        counter += 1
 
 
-            speed_tab, distance_tab = st.tabs(["Speed Over Time", "Distance Over Time"])
+                data['Status Byte'] = data['Status Byte'].astype(str)
 
 
+                speed_tab, distance_tab = st.tabs(["Speed Over Time", "Distance Over Time"])
 
-            fig = px.scatter(data,
-                             x='Timestamp',
-                             y='Speed',
-                             hover_data=['Status Byte'],
-                             labels={
-                                 "Speed": "Speed (km/h)",
-                                 "Timestamp": "Date",
+                with speed_tab:
+                    fig = px.scatter(data,
+                                     x='Timestamp',
+                                     y='Speed',
+                                     hover_data=['Status Byte'],
+                                     labels={
+                                         "Speed": "Speed (km/h)",
+                                         "Timestamp": "Date",
 
-                             }, )
-            fig.update_traces(mode="lines")
+                                     }, )
+                    fig.update_traces(mode="lines")
 
-            with speed_tab:
-                st.plotly_chart(fig, use_container_width=True)
-            with distance_tab:
-                fig = px.scatter(data,
-                                 x='Timestamp',
-                                 y='Distance',
-                                 hover_data=['Status Byte'],
-                                 labels={
-                                     "Distance": "Distance (meter)",
-                                     "Timestamp": "Date",
-                                 },
-                                 )
-                fig.update_traces(mode="lines")
-                st.plotly_chart(fig, use_container_width=True)
+
+                    st.plotly_chart(fig, use_container_width=True)
+                with distance_tab:
+                    fig = px.scatter(data,
+                                     x='Timestamp',
+                                     y='Distance',
+                                     hover_data=['Status Byte'],
+                                     labels={
+                                         "Distance": "Distance (meter)",
+                                         "Timestamp": "Date",
+                                     },
+                                     )
+                    fig.update_traces(mode="lines")
+                    st.plotly_chart(fig, use_container_width=True)
+
+                st.dataframe(data_first, use_container_width=True)
+            else:
+                st.write("PLEASE LOAD A DRS05 CSV FILE.")
         except:
             st.write("PLEASE LOAD A SUITABLE CSV FILE.")
 
@@ -127,83 +131,88 @@ with dr42_tab:
     for uploaded_file_dr42 in uploaded_files_dr42:
         try:
             data = pd.read_csv(uploaded_file_dr42)
-            st.write("filename:", uploaded_file_dr42.name)
+            if (data["Radar Message"][0][0] == "#"):
+                data_first = data.copy()
+                st.write("filename:", uploaded_file_dr42.name)
 
-            data["Speed"] = 0
-            data["Distance"] = 0
-            data["Status Byte"] = 0
+                data["Speed"] = 0
+                data["Distance"] = 0
+                data["Status Byte"] = 0
 
-            for i in range(len(data)):
-                speed = data["Radar Message"][i][5:9]
-                speed = int(speed, base=16)
+                for i in range(len(data)):
+                    speed = data["Radar Message"][i][5:9]
+                    speed = int(speed, base=16)
 
-                speed = (speed * 600) / 65535
+                    speed = (speed * 600) / 65535
 
-                data["Speed"][i] = speed
+                    data["Speed"][i] = speed
 
-                distance_counter = data["Radar Message"][i][9:13]
-                distance_counter = int(distance_counter, base=16)
-                distance_counter = distance_counter / 10
-                data["Distance"][i] = distance_counter
+                    distance_counter = data["Radar Message"][i][9:13]
+                    distance_counter = int(distance_counter, base=16)
+                    distance_counter = distance_counter / 10
+                    data["Distance"][i] = distance_counter
 
-                status_byte = data["Radar Message"][i][23:25]
-                ini_string = status_byte
-                n = int(ini_string, 16)
-                bStr = ''
-                while n > 0:
-                    bStr = str(n % 2) + bStr
-                    n = n >> 1
-                res = bStr
+                    status_byte = data["Radar Message"][i][23:25]
+                    ini_string = status_byte
+                    n = int(ini_string, 16)
+                    bStr = ''
+                    while n > 0:
+                        bStr = str(n % 2) + bStr
+                        n = n >> 1
+                    res = bStr
 
-                data["Status Byte"][i] = res
+                    data["Status Byte"][i] = "0x" + data["Radar Message"][i][23:25] + ", " + "0b" + res
 
-            counter = 0
+                counter = 0
 
-            for j in list(data["Timestamp"].unique()):
-                for i in range(len(data.loc[data['Timestamp'] == j])):
-                    time = data["Timestamp"][counter].split(" ")[3].split(":")
-                    split_second = 60 / (len(data.loc[data['Timestamp'] == j]) + i)
-                    date = datetime.datetime(int(data["Timestamp"][counter].split(" ")[-1]),
-                                             2,
-                                             int(data["Timestamp"][counter].split(" ")[2]),
-                                             int(time[0]),
-                                             int(time[1]),
-                                             int(time[2]),
-                                             int((split_second * 10000) * i))
+                for j in list(data["Timestamp"].unique()):
+                    for i in range(len(data.loc[data['Timestamp'] == j])):
+                        time = data["Timestamp"][counter].split(" ")[3].split(":")
+                        split_second = 60 / (len(data.loc[data['Timestamp'] == j]) + i)
+                        date = datetime.datetime(int(data["Timestamp"][counter].split(" ")[-1]),
+                                                 2,
+                                                 int(data["Timestamp"][counter].split(" ")[2]),
+                                                 int(time[0]),
+                                                 int(time[1]),
+                                                 int(time[2]),
+                                                 int((split_second * 10000) * i))
 
-                    data["Timestamp"][counter] = date
+                        data["Timestamp"][counter] = date
 
-                    counter += 1
+                        counter += 1
 
-            data['Status Byte'] = data['Status Byte'].astype(str)
 
-            speed_tab, distance_tab = st.tabs(["Speed Over Time", "Distance Over Time"])
 
-            fig = px.scatter(data,
-                             x='Timestamp',
-                             y='Speed',
-                             hover_data=['Status Byte'],
-                             labels={
-                                 "Speed": "Speed (km/h)",
-                                 "Timestamp": "Date",
+                speed_tab, distance_tab = st.tabs(["Speed Over Time", "Distance Over Time"])
 
-                             }, )
-            fig.update_traces(mode="lines")
-
-            with speed_tab:
-                st.plotly_chart(fig, use_container_width=True)
-            with distance_tab:
                 fig = px.scatter(data,
                                  x='Timestamp',
-                                 y='Distance',
+                                 y='Speed',
                                  hover_data=['Status Byte'],
                                  labels={
-                                     "Distance": "Distance (meter)",
+                                     "Speed": "Speed (km/h)",
                                      "Timestamp": "Date",
-                                 },
-                                 )
-                fig.update_traces(mode="lines")
-                st.plotly_chart(fig, use_container_width=True)
 
+                                 }, )
+                fig.update_traces(mode="lines")
+
+                with speed_tab:
+                    st.plotly_chart(fig, use_container_width=True)
+                with distance_tab:
+                    fig = px.scatter(data,
+                                     x='Timestamp',
+                                     y='Distance',
+                                     hover_data=['Status Byte'],
+                                     labels={
+                                         "Distance": "Distance (meter)",
+                                         "Timestamp": "Date",
+                                     },
+                                     )
+                    fig.update_traces(mode="lines")
+                    st.plotly_chart(fig, use_container_width=True)
+
+                st.dataframe(data_first, use_container_width=True)
+            else:
+                st.write("PLEASE LOAD A DR42 CSV FILE.")
         except:
             st.write("PLEASE LOAD A SUITABLE CSV FILE.")
